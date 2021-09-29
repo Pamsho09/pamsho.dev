3d8962c02c9f0287f8de2ab69ba73863a98add17
pamsho <francsico@tinkin.one>
2021-09-28 23:32:09 -0500

    new version of pamsho.dev add notification

--- pages/_app.js
+++ pages/_app.js
@@ -8,6 +8,8 @@ const MyApp = ({ Component, pageProps }) => {
     OneSignal.push(() => {
       OneSignal.init({
         appId: '93b47c56-e8fa-434e-a102-06994dcf584c',
+        safari_web_id:
+          'web.onesignal.auto.2fe112ef-4554-4496-8fef-42f5ec2b9118',
         notifyButton: {
           enable: true,
         },
