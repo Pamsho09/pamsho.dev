4a299903b598515c930f212d777df3c39fb024c9
pamsho <francisco@tinkin.one>
2022-06-13 21:07:44 -0500

    fix div

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,7 @@ import { useAccessRoute } from 'hook/api/useLogin'
 const Layout = ({ children }: any): JSX.Element => {
   useAccessRoute()
   return (
-    <div>
+    <div className="w-full max-w-[1152px] m-auto min-h-screen flex flex-col pl-5 pr-5 ">
       {children}
       <Footer />
     </div>
